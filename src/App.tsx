bfe73be2065a8f57e00df92d6f892ab2c7d7c558
rickypcyt <rickypcyt@gmail.com>
2025-09-08 20:36:50 +0200

    Hello World

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      fontSize: '3rem',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      width: '100%'
+    }}>
+      Hello World
+    </div>
+  )
+}
+
+export default App
